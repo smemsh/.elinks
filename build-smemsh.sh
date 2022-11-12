549c43630c00fd1dccbd9146e541acd549054eb8
Scott Mcdermott <scott@smemsh.net>
2022-11-12 11:17:08 -0800

    - meson: switch from autoconf to meson as the supported build system
    upstream developer prefers meson and said basically autoconf is
    deprecated
    
    fiddled with some options too, audited defaults from meson_options.txt

---
+++ build-smemsh.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -x
+bomb () { echo "meson $@ failed" >&2; false; exit; }
+
+test -d build && rm -rf build
+meson setup build || bomb build
+
+# see meson_options.txt, these are only non-default vaules
+meson configure \
+	-D prefix=/usr/local \
+	-D sysconfdir=/etc/elinks \
+	-D ipv6=false \
+	-D bittorrent=false \
+	-D mouse=false \
+	-D 88-colors=true \
+	-D 256-colors=true \
+	-D exmode=true \
+	-D html-highlight=true \
+	-D fastmem=true \
+	-D gpm=false \
+	-D terminfo=true \
+	-D zstd=true \
+	-D brotli=true \
+	-D python=true \
+	-D libevent=true \
+	-D no-root=true \
+build || bomb configure
+
+cd build && ninja && cd - || bomb ninja
+
+echo "completed."
+echo "to install: sudo ninja -C build install"
