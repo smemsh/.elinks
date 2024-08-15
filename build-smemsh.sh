6e02ebab72241f465bc3d196f33ff883cea789d5
Scott Mcdermott <scott@smemsh.net>
2024-08-15 03:50:23 -0700

    / build: netsurf: install: install instructions had a bad extglob
    need to exclude share itself, otherwise it globs.  it's empty anyways

--- build-smemsh.sh
+++ build-smemsh.sh
@@ -96,7 +96,7 @@ then (
 	cat <<- %
 	build done
 	sudo cp -uvr \
-	netsurf/nsbuild/inst-x86_64-linux-gnu/!(share/netsurf-buildsystem) \
+	netsurf/nsbuild/inst-x86_64-linux-gnu/!(share) \
 	/usr/local/
 	%
 	read -n1 -p 'waiting for key to continue when done...'
